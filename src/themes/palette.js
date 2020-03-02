7e66a331ec32e462746fd82c6c81af26b16b22d0
parkoon <jh.park@qualson.com>
2020-03-02 17:47:42 +0900

    init project

---
+++ src/themes/palette.js
@@ -0,0 +1,25 @@
+const palette = {
+  primary: "#312a75",
+  secondary: "#6439fd",
+  white: "#fff",
+  danger: "#f75d5e",
+  success: "#50e3c2",
+  black: "",
+  grayscale: [
+    "#212121",
+    "#414141",
+    "#616161",
+    "#9e9e9e",
+    "#bdbdbd",
+    "#e0e0e0",
+    "#eeeeee",
+    "#ffffff"
+  ],
+
+  hover: {
+    primary: "tomato",
+    secondary: "green"
+  }
+};
+
+export default palette;
