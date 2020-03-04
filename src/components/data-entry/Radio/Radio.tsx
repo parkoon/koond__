e43114aa330c30de67d8a35253d83cfb9374a1a8
parkoon <jh.park@qualson.com>
2020-03-04 18:05:05 +0900

    [기능] radio

---
+++ src/components/data-entry/Radio/Radio.tsx
@@ -0,0 +1,62 @@
+import React from 'react'
+import styled from 'styled-components'
+import palette from '../../../themes/palette'
+
+const StyledRadio = styled.input`
+  display: none;
+
+  &:checked + span::after {
+    opacity: 1;
+    transform: translate(-50%, -50%) scale(1);
+  }
+`
+
+const StyledRadioIcon = styled.span`
+  display: inline-block;
+  width: 17px;
+  height: 17px;
+  box-sizing: border-box;
+  vertical-align: middle;
+  border-radius: 50%;
+  border: 1px solid ${palette.primary};
+  position: relative;
+
+  &::after {
+    content: '';
+    position: absolute;
+    width: 9px;
+    height: 9px;
+    border-radius: 50%;
+    box-sizing: border-box;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%) scale(0);
+    background: ${palette.primary};
+    opacity: 0;
+    transition: 0.2s ease;
+  }
+`
+
+const StyledRadioText = styled.span`
+  margin-left: 5px;
+`
+
+const StyledLabel = styled.label`
+  cursor: pointer;
+`
+
+type RadioProps = {
+  children: string
+}
+
+function Radio({ children }: RadioProps) {
+  return (
+    <StyledLabel>
+      <StyledRadio type="radio"></StyledRadio>
+      <StyledRadioIcon />
+      <StyledRadioText>{children}</StyledRadioText>
+    </StyledLabel>
+  )
+}
+
+export default Radio
---
+++ src/components/data-entry/Radio/index.stories.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import { Radio, RadioGroup } from './'
+
+export default {
+  title: 'Data Entry',
+  component: Radio,
+}
+
+export const radio = () => {
+  return (
+    <>
+      <Radio>Radio</Radio>
+    </>
+  )
+}
