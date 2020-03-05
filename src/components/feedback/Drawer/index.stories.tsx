50ebb11db292cb3813b463e4e483ad0999e22e65
parkoon <jh.park@qualson.com>
2020-03-05 10:24:56 +0900

    [기능] init drawer

---
+++ src/components/feedback/Drawer/index.stories.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Drawer from './Drawer'
+
+export default {
+  title: 'Feedback',
+  component: Drawer,
+}
+
+export const drawer = () => {
+  return (
+    <>
+      <Drawer />
+    </>
+  )
+}
