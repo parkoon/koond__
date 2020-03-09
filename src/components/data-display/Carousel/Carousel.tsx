5cbabad548e1fdb976716ea5e7bcc761b5a0394d
parkoon <jh.park@qualson.com>
2020-03-09 23:00:35 +0900

    [기능] init carousel

---
+++ src/components/data-display/Carousel/Carousel.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+function Carousel() {
+  return <div>CCC</div>
+}
+
+export default Carousel
---
+++ src/components/data-display/Carousel/index.stories.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Carousel from './index'
+export default {
+  title: 'Data Display',
+  component: Carousel,
+}
+
+export const carousel = () => {
+  return (
+    <>
+      <Carousel />
+    </>
+  )
+}
