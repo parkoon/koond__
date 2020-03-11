10d6d22b7625140879a412a2683325d791939d53
parkoon <jh.park@qualson.com>
2020-03-11 23:28:27 +0900

    delete useless code

--- src/components/data-display/Badge/index.stories.tsx
+++ src/components/data-display/Badge/index.stories.tsx
@@ -9,7 +9,7 @@ export default {
 export const badge = () => {
   return (
     <>
-      <Badge count={0} showZero={false} />
+      <Badge count={0} />
       <Badge count={2} />
       <Badge count={20} style={{ background: '#ff7675' }} />
       <Badge count={100} overflowCount={10} style={{ background: '#ff7675' }} />
