bf0469d24815a9e2e92e3f1bf584dd1c954bccfd
parkoon <jh.park@qualson.com>
2020-03-11 15:59:29 +0900

    [형식] delete meta component

--- src/components/data-display/Card/Meta.tsx
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-function Meta() {
-  return <div>Meta</div>
-}
-
-export default Meta
--- src/components/data-display/Card/index.ts
+++ src/components/data-display/Card/index.ts
@@ -1,5 +1,3 @@
 import Card from './Card'
-import Meta from './Meta'
 
-Card.Meta = Meta
 export default Card
