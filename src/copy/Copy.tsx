3f9727717c3e21cb4e7b84cf0d84d35a89c400a1
parkoon <jh.park@qualson.com>
2020-03-12 23:19:23 +0900

    [형식] copy code

---
+++ src/copy/Copy.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+import * as Styled from './styled'
+import * as I from './interface'
+
+function Copy() {
+  return <div></div>
+}
+
+export default Copy
---
+++ src/copy/index.stories.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Copy from '.'
+
+export default {
+  title: '',
+}
+
+export const copy = () => {
+  return (
+    <>
+      <Copy />
+    </>
+  )
+}
