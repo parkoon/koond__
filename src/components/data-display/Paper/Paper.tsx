ecbbcd4976a63634bbe5e777c8d977e4491f9330
parkoon <jh.park@qualson.com>
2020-03-16 19:17:02 +0900

    [형식] update paper storybook

--- src/components/data-display/Paper/Paper.tsx
+++ src/components/data-display/Paper/Paper.tsx
@@ -3,34 +3,6 @@ import styled from 'styled-components'
 import { ambient, penumbra, umbra } from './evaluation'
 import palette from '../../../themes/palette'
 
-const EVALUATION_MAP = {
-  0: '0px 0px 0px 0px',
-  1: '0px 2px 1px -1px',
-  2: '0px 3px 1px -2px',
-  3: '0px 3px 3px -2px',
-  4: '0px 2px 4px -1px',
-  5: '0px 3px 5px -1px',
-  6: '0px 3px 5px -1px',
-  7: '0px 4px 5px -2px',
-  8: '0px 5px 5px -3px',
-  9: '0px 5px 6px -3px',
-  10: '0px 6px 6px -3px',
-  11: '0px 6px 7px -4px',
-  12: '0px 7px 8px -4px',
-  13: '0px 7px 8px -4px',
-  14: '0px 7px 9px -4px',
-  15: '0px 8px 9px -5px',
-  16: '0px 8px 10px -5px',
-  17: '0px 8px 11px -5px',
-  18: '0px 9px 11px -5px',
-  19: '0px 9px 12px -6px',
-  20: '0px 10px 13px -6px',
-  21: '0px 10px 13px -6px',
-  22: '0px 10px 14px -6px',
-  23: '0px 11px 14px -7px',
-  24: '0px 11px 15px -7px',
-}
-
 const StyledPaper = styled.div<PaperProps>`
   display: inline-block;
   position: relative;
@@ -48,8 +20,14 @@ const StyledPaper = styled.div<PaperProps>`
 type PaperProps = {
   children?: React.ReactNode
   style?: React.CSSProperties
+
+  /** 그림자의 정도 */
   evaluation?: number
+
+  /** 테두리 여부 */
   outlined?: boolean
+
+  /** 사각형으로 표현 할 것인가 */
   square?: boolean
 }
 
--- src/components/data-display/Paper/index.stories.tsx
+++ src/components/data-display/Paper/index.stories.tsx
@@ -2,15 +2,29 @@ import React from 'react'
 import Paper from './'
 
 export default {
-  title: 'Data Display',
+  title: 'Paper',
   component: Paper,
 }
 
-export const paper = () => {
+export const Default = () => {
   return (
     <>
-      <Paper style={{ width: '128px', height: '128px' }} outlined></Paper>
+      <Paper style={{ width: '128px', height: '128px' }}></Paper>
+      <Paper style={{ width: '128px', height: '128px' }} evaluation={4}></Paper>
+      <Paper style={{ width: '128px', height: '128px' }} evaluation={7}></Paper>
+    </>
+  )
+}
+export const Outlined = () => {
+  return (
+    <>
       <Paper style={{ width: '128px', height: '128px' }} evaluation={4} outlined></Paper>
+    </>
+  )
+}
+export const Square = () => {
+  return (
+    <>
       <Paper style={{ width: '128px', height: '128px' }} evaluation={7} square></Paper>
     </>
   )
