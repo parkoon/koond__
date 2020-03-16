201757cad59c6682f01ab4be3c3bc36b2702ebd0
parkoon <jh.park@qualson.com>
2020-03-16 20:49:16 +0900

    [형식] update checkbox storybook

--- src/components/data-entry/Checkbox/Checkbox.tsx
+++ src/components/data-entry/Checkbox/Checkbox.tsx
@@ -1,11 +1,37 @@
 import React, { useState } from 'react'
 
 import * as Styled from './styled'
-import * as I from './interface'
 
 import CheckboxGroup from './Group'
 
-function Checkbox({ children, onChange, checked, title, ...props }: I.CheckBoxProps) {
+export type Option = {
+  label: string
+  value: string
+}
+
+export type CheckBoxProps = {
+  /** 체크박스에 사용될 텍스트 */
+  children?: React.ReactNode
+
+  /** 체크되어 있는지 여부 */
+  checked?: boolean
+
+  /** 체크박스 이름 */
+  name: string
+
+  /** 체크박스 상단에 사용할 타이틀 */
+  title?: React.ReactNode
+  onChange: (e: React.ChangeEvent<HTMLInputElement>) => void
+}
+
+export type CheckBoxGroupProps = {
+  options: Option[]
+  defaultValue: string[]
+  title?: React.ReactNode
+  onChange: (values: string[]) => void
+}
+
+function Checkbox({ children, onChange, checked, title, ...props }: CheckBoxProps) {
   console.log('children', children)
   const [_checked, setChecked] = useState(checked)
   const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
--- src/components/data-entry/Checkbox/index.stories.tsx
+++ src/components/data-entry/Checkbox/index.stories.tsx
@@ -1,29 +1,27 @@
 import React from 'react'
 import Checkbox from './Checkbox'
-import { withKnobs } from '@storybook/addon-knobs'
 
 export default {
-  title: 'Data Entry',
+  title: 'Checkbox',
   component: Checkbox,
-  decorators: [withKnobs],
 }
 
-const options = [
-  {
-    label: 'Apple',
-    value: 'Apple',
-  },
-  {
-    label: 'Pear',
-    value: 'Pear',
-  },
-  {
-    label: 'Orange',
-    value: 'Orange',
-  },
-]
+export const Default = () => {
+  const options = [
+    {
+      label: 'Apple',
+      value: 'Apple',
+    },
+    {
+      label: 'Pear',
+      value: 'Pear',
+    },
+    {
+      label: 'Orange',
+      value: 'Orange',
+    },
+  ]
 
-export const checkbox = () => {
   const handleChange = e => {
     console.log(e.target.checked)
   }
