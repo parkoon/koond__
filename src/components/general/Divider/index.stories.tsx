c5b7718bf6e91ce4a1ef39dc620f2f5e9d5117b5
parkoon <jh.park@qualson.com>
2020-03-16 17:48:32 +0900

    [형식] update divider story

--- src/components/general/Divider/index.stories.tsx
+++ src/components/general/Divider/index.stories.tsx
@@ -2,11 +2,14 @@ import React from 'react'
 import Divider from '.'
 
 export default {
-  title: 'General',
+  title: 'Divider',
   component: Divider,
+  parameters: {
+    componentSubtitle: '블럭 간 선을 나눌 때 사용하는 컴포넌트',
+  },
 }
 
-export const divider = () => {
+export const Default = () => {
   return (
     <>
       <p>
@@ -14,6 +17,23 @@ export const divider = () => {
         sunt a te dicta? Refert tamen, quo modo.
       </p>
       <Divider />
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed nonne merninisti licere mihi ista probare, quae
+        sunt a te dicta? Refert tamen, quo modo.
+      </p>
+      <Divider />
+    </>
+  )
+}
+
+export const Dashed = () => {
+  return (
+    <>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed nonne merninisti licere mihi ista probare, quae
+        sunt a te dicta? Refert tamen, quo modo.
+      </p>
+      <Divider dashed />
       <p>
         Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed nonne merninisti licere mihi ista probare, quae
         sunt a te dicta? Refert tamen, quo modo.
