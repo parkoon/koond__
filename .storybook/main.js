0aa7e6e517500b4441cdf087db5f0240a15f04cf
parkoon <jh.park@qualson.com>
2020-03-16 15:02:00 +0900

    [기능] add story addon

--- .storybook/main.js
+++ .storybook/main.js
@@ -3,7 +3,8 @@ module.exports = {
   addons: [
     '@storybook/preset-create-react-app',
     '@storybook/addon-knobs/register',
-    '@storybook/addon-actions',
+    '@storybook/addon-storysource',
+    // '@storybook/addon-actions',
     '@storybook/addon-links',
     '@storybook/addon-docs/react/preset',
   ],
